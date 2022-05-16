f03762764e743c2f3da716dd3d6d6a9b9fb426d4
jzc <jzc@qq.com>
2022-05-16 18:58:00 +0800

    day3 commit

---
+++ day3/gee/gee.go
@@ -0,0 +1,36 @@
+package gee
+
+import "net/http"
+
+//框架入口
+
+type HandlerFunc func(*Context)
+
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := newContext(w, r)
+	engine.router.handle(c)
+}
---
+++ day3/gee/trie.go
@@ -0,0 +1,88 @@
+package gee
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 添加trie树的目的是实现动态路由
+// 前缀树的两个基本操作：1.insert； 2.search
+type node struct {
+	pattern  string  //待匹配路由
+	part     string  //路由的一部分
+	children []*node //子节点
+	isWild   bool    //part是否含有：或者 *
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
+
+// 插入
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+// 查找
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
+
+// 遍历树， 返回所有的叶子节点（即所有的路由）
+func (n *node) travel(list *([]*node)) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+// 返回第一个匹配的子节点, 用于插入操作
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// 返回所有匹配的子节点，用于查询操作 (这里不止一个的原因是 有“：” 和 ”*“ 这两个通配符)
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
